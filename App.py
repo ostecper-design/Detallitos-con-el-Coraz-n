5ccb369a4913ce2c088db02df38e3e5ce6968bb0
ostecper-design <ostec.per@gmail.com>
2026-09-05 21:51:44 -0600

    Change data source URL and enhance error handling
    Updated the data loading function to use a new URL for CSV download and improved error handling.

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Cotizador Móvil", page_icon="🧮", layout="cent
 st.title("📱 Cotizador en la Nube")
 
 # TU ENLACE CORRECTO CONFIGURADO PARA DESCARGA DIRECTA
-URL_UNIVERSAL = "https://google.com"
+URL_UNIVERSAL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGZnZYeCUJkXq3VThyrroJYOxCHiZEgV4RiGxclb2rSjZWAe2wwx8GOjb8wlefj9YsrZpO6jZMeVAR/pub?output=csv"
 # 1. Cargar base de datos de forma directa y limpia
 @st.cache_data(ttl=2)
 def cargar_datos():
